bae84dcd5602fe5dcf83854eecc6acdc3ffd0899
mpeck516 <15614402@students.lincoln.ac.uk>
2019-03-05 12:50:13 +0000

    game
    game

---
+++ Menu/Assets/scripts/playerScript.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class playerScript : MonoBehaviour
+{
+    public int playerHealth = 100;
+    public int bronzeSwordDamage = 15;
+    public int playerDamage = 10;
+
+    bool bronzeSwordUp = false;
+
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        if(bronzeSwordUp == true)
+        {
+            int totalDamage = playerDamage + bronzeSwordDamage;
+        }
+    }
+}
---
+++ Menu/Assets/scripts/turnManager.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class turnManager : MonoBehaviour
+{
+    bool playerTurn = true;
+    bool enemyTurn = false;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+                
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (playerTurn == true)
+        {
+            print("player turn");
+            if (Input.GetKey("a"))
+            {
+                print("a pressed");
+                playerTurn = false;
+                enemyTurn = true;
+            }
+            //if (Input.GetKey("space"))
+            //{
+            //    print("player attacks");
+
+            //}
+        }
+        if (enemyTurn == true)
+        {
+            print("enemy turn");
+            if (Input.GetKey("b"))
+            {
+                print("a pressed");
+                playerTurn = true;
+                enemyTurn = false;
+            }
+        }
+        
+    }
+}
