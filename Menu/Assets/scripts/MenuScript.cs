8862d11ca50afc2cf2642ba2863f3689086cac09
Turbanator7000 <adamharrod1998@hotmail.co.uk>
2019-03-13 13:32:36 +0000

    MenuUpdated
    Added a functioning main menu system, requires settings to be filled in still and background to be added.

--- Menu/Assets/scripts/MenuScript.cs
+++ Menu/Assets/scripts/MenuScript.cs
@@ -3,13 +3,71 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
-
 public class MenuScript : MonoBehaviour
 {
+    int currVar = 1;
+    bool toggle = true;
 
-public void SceneLoader(int SceneIndex)
+    public GameObject start;
+    public GameObject options;
+    public GameObject end;
+
+    public void FixedUpdate()
     {
-        SceneManager.LoadScene(SceneIndex);
+        if (toggle == true)
+            {
+                StartCoroutine(toggling());
+                if (currVar == 1)
+                {
+                    start.SetActive(true);
+                    options.SetActive(false);
+                    end.SetActive(false);
+                }
+                if (currVar == 2)
+                {
+                    start.SetActive(false);
+                    options.SetActive(true);
+                    end.SetActive(false);
+                }
+                if (currVar == 3)
+                {
+                    start.SetActive(false);
+                    options.SetActive(false);
+                    end.SetActive(true);
+                }
+            }
+            if (Input.GetKeyDown("space") && currVar == 1)
+            {
+                SceneManager.LoadScene("battleScene");
+            }
+            if (Input.GetKeyDown("space") && currVar == 2)
+            {
+                SceneManager.LoadScene("optionsMenu");
+            }
+            if (Input.GetKeyDown("space") && currVar == 3)
+            {
+                Application.Quit();
             }
     }
+
+IEnumerator toggling()
+    {
+        toggle = false;
+        if (Input.GetAxis("Vertical") > 0)
+        {
+            currVar--;
+        }
+        if (Input.GetAxis("Vertical") < 0)
+        {
+            currVar++;
+        }
+        if (currVar > 3)
+            currVar = 1;
+        if (currVar < 1)
+            currVar = 3;
+
+        yield return new WaitForSeconds(0.3f);
+        toggle = true;
+    }
+}
 	
