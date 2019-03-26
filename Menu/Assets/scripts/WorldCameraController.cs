43b0e1f0a3352e407bd17cf0836327eefca6badc
Luke Howard <lukealechoward@hotmail.com>
2019-03-26 16:36:39 +0000

    Improved Camera
    Added WorldCameraController script to allow player to navigate the level.

---
+++ Menu/Assets/scripts/WorldCameraController.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WorldCameraController : MonoBehaviour
+{
+    public GameObject player;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        Vector3 newPos = new Vector3(player.transform.localPosition.x, 0.0f, -10.0f);
+        transform.position = newPos;
+    }
+}
